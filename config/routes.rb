2821ccd1e4ebaf4a479ad59f7d41d786ed2db74a
bbborisk <bbborisk@gmail.com>
2020-01-23 15:56:28 +0100

    Created users controller, my books blank page, added book model and personal page named 'my_books'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "welcome#index"
+  get "my_books", to: "users#my_books"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_134425) do
+ActiveRecord::Schema.define(version: 2020_01_23_143519) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
